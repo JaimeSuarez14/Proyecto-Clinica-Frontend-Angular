2036d6ba1fdde3fefb60a0f6ee1ed130dafc2e6e
Jaime Suarez <jaisua8@gmail.com>
2026-02-20 17:08:37 -0500

    creacion del menu principal con los iconos de lucide-icon

--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, UserRound , House, Hospital } from 'lucide-angular';
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  readonly userRound  = UserRound ;
+  readonly  house = House;
+  readonly  hospital = Hospital;
 
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, CalendarArrowDown , CalendarArrowUp , Users , IdCard } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+
+  readonly menu = [
+    {
+      icon : IdCard,
+      name : "Medicos",
+      route : "/medicos",
+      active : true
+    },
+    {
+      icon : Users ,
+      name : "Paciente",
+      route : "/medicos",
+      active : true
+    },
+    {
+      icon : CalendarArrowUp ,
+      name : "Ingresos",
+      route : "/medicos",
+      active : true
+    },
+    {
+      icon : CalendarArrowDown ,
+      name : "Egresos",
+      route : "/medicos",
+      active : true
+    }
+
+  ]
+
 
 }
